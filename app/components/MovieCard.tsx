67cb6f3051e643bdd55a965e7f18877fd69f93f7
O'Carroll <mocarroll@doverfoodretail.com>
2025-04-29 13:56:13 -0500

    got api call running and displaying relevant data

--- app/components/MovieCard.tsx
+++ app/components/MovieCard.tsx
@@ -1,14 +1,39 @@
-import React from 'react';
+'use client'
 
-export  default function MovieCard(){
-    return(
-        <h1 className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm">
-        <p className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          This Content is in the MovieCard Component
-        </p>
-        <p className="font-normal text-gray-700 dark:text-gray-400">
-          (pull movie information from https://www.omdbapi.com/)
-        </p>
+import { useState, useEffect } from "react";
+
+
+// set my API URL
+const URL = 'https://www.omdbapi.com/?i=tt3896198&apikey=' + process.env.NEXT_PUBLIC_API_KEY
+
+export default function MovieCard() {
+  const [movieTitle, setMovieTitle] = useState(0)
+  const [runTime, setRunTime] = useState(0)
+  const [director, setDirector] = useState(0)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(URL)
+      result.json().then(json => {
+        console.log(json)
+        setMovieTitle(json.Title);
+        setRunTime(json.Runtime);
+        setDirector(json.Director);
+      })
+      
+    }
+    fetchData();
+  },[])
+
+  return (
+    <div>
+      <h1 className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm">
+      <p className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Title: {movieTitle} </p>
+      <p>Director: {director}</p>
+      <p>Runtime: {runTime} </p>
+      <p>Showtimes: 3pm / 6pm / 9pm</p>
       </h1>
-    )
+    </div>
+  
+  );
 }
